153e5784c9c9540f21b08d9069130d1fd0fb015b
Anderson Karl <42244540+Artoshk@users.noreply.github.com>
2024-06-21 21:46:37 -0300

    dockerfile

--- src/main_example.py
+++ src/main_example.py
@@ -1,18 +1,27 @@
 import gradio as gr
 from transformers import pipeline
 import numpy as np
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# The modes was stored on C:\Users\{user}\.cache\huggingface\hub\
 transcriber = pipeline(
     "automatic-speech-recognition", 
-    model="openai/whisper-base",
+    model="openai/whisper-small",
+    # Lighter models if you don't have a GPU, from heaviest to lightest 
+    # whisper-small > whisper-base > whisper-tiny
     generate_kwargs={"language": "portuguese"},
-    device="cpu",
-    batch_size=8,
+    device=device,
+    batch_size=1,
     chunk_length_s=30,
     framework="pt"
     )
 
-def transcribe(audio):
+def transcribe(audio) -> str:
+    if audio is None:
+        return ""
+    
     sr, y = audio
     y = y.astype(np.float32)
     y /= np.max(np.abs(y))
